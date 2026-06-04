f27a2a4e578810f894f053acb17521074f8a461d
Rudra8948 <asaditya0563@gmail.com>
2026-06-04 06:54:15 +0000

    data types

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,32 @@
+"use strict"; //treat all js code as newer version
+
+//alert (3+3)// we are using nodejs ,not browser
+
+console.log(3+3)
+console.log("rudra")
+
+
+let name = "rudra"
+let age = "21"
+let isLoggedIn = "false"
+
+//number =>2 to power 53
+//bigint
+//string=>""
+//boolean=> true/false
+// null => standalone value
+//undefine=> 
+// symbol => unique
+
+
+//object
+
+console.log(typeof undefined); // undefined 
+console.log(typeof null);   // object
+console.log(typeof 10);        // "number"
+console.log(typeof "Hello");   // "string"
+console.log(typeof true);      // "boolean"
+console.log(typeof undefined); // "undefined"
+console.log(typeof null);      // "object" (JavaScript ka known bug)
+
+
