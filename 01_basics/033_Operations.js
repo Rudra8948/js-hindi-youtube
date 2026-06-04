1888c6b5ad068a39c9513638d738c56899b92c37
Rudra8948 <asaditya0563@gmail.com>
2026-06-04 23:53:36 +0000

    Operations

---
+++ 01_basics/033_Operations.js
@@ -0,0 +1,38 @@
+let value =3
+let negValue = -value
+//console.log(negValue);
+
+/* console.log(2+2);
+console.log(2-2);
+console.log(2*2);
+console.log(2**3);
+console.log(2/3);
+console.log(2%3);
+*/
+
+let str1 = "Aditya"
+let str2 = " Singh"
+let str3 = str1 + str2
+console.log(str3);
+
+console.log("1"+2);  //12
+console.log(1+"2");   //12
+console.log("1"+2+2);  //122
+console.log(1+2+"2");  //32
+
+console.log(true);  // true
+console.log(+true);  //1
+console.log(+"");    //0
+
+let gameCounter = 100
+gameCounter++;
+console.log(gameCounter);   // 101
+// link to study 
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Increment
+
+
+
+
+
+
+
