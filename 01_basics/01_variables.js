b1ac95a5d8a40bf6ec5a0344e1af4ad0d7255eaa
Rudra8948 <asaditya0563@gmail.com>
2026-06-04 06:29:55 +0000

    hello

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,21 @@
+const accountId = 12345
+let accountEmail = "rudra89@gmail.com"
+var accountPassword = "12340"
+accountCity = "Luckow"
+let accountState;
+
+//accountId = 2 // not allowed
+ accountEmail = "hdfcrudra@gmail.com"
+ accountPassword ="8948"
+ accountCity = "delhi"
+
+
+ console.log(accountId);
+ /* 
+prefer not to use var 
+bcz of issues in block scope amd functional scope
+
+*/
+
+console.table([accountEmail,accountPassword,accountCity,accountState])
+
