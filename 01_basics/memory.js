6539738d3e9df51f92cdc5fdbe40f4fe1ad1b85e
Rudra8948 <asaditya0563@gmail.com>
2026-06-05 08:50:28 +0000

    heap and stack

---
+++ 01_basics/memory.js
@@ -0,0 +1,20 @@
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+  //stack(Primitive)
+  //heap(Non-primitive)
+
+  let MyYoutubename = "Rudrasinghdot.com"
+  let anothername = MyYoutubename
+  anothername = "Aditysinghdot.com"
+  console.log(anothername);
+  console.log(MyYoutubename);
+
+  let userOne={
+    email:"user@google.com",
+    upi:"user@hdfc",
+  }
+
+  let userTwo = userOne
+  userTwo.email ="rudrasingh@hamil.com"
+ 
+  console.log(userOne.email);
+  console.log(userTwo.email);
